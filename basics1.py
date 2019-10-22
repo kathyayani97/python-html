18da4e807badd8f5bcc34df24494b7a473064dcb
kathyayani97 <kathyayanic@unifytech.com>
2019-10-22 15:06:45 +0530

    last part

--- basics1.py
+++ basics1.py
@@ -8,22 +8,16 @@ myclient = pymongo.MongoClient("localhost", 27017)
 mydb = myclient.people
 mycol = mydb.html
 
-
+links_list = []
 
 url = "http://www.google.co.in"
 
 page = requests.get(url)
 data = page.text
-soup = BeautifulSoup(data, 'html.parser')
-
-
+soup = BeautifulSoup(data,'html.parser')
 
 for link in soup.find_all('a'):
-    print(link.get('href'))
-    #mycol.insert_one(link)
-
-
-
-
-
-
+    links_list.append(link.get('href'))
+    # print(link.get('href'))
+mycol.insert_one({"links":links_list})
+# print(links_list)
